e0fcb9ac9747ada1b94dfd80903642c560dce96d
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-02 13:16:47 -0300

    New entity: telegram config

---
+++ src/api/telegram-config/controllers/telegram-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  telegram-config controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::telegram-config.telegram-config');
---
+++ src/api/telegram-config/routes/telegram-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * telegram-config router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::telegram-config.telegram-config');
