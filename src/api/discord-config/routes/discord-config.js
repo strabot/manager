3c0840e91e1b7d0f50c66847351c8a7ba2d26f09
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-10 11:02:47 -0300

    Initial discord config

---
+++ src/api/discord-config/routes/discord-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * discord-config router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::discord-config.discord-config');
