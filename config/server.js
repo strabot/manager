a3a2ce2856980160c7a100f641f9286fd771bcb1
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-17 17:09:48 -0300

    Using environment variables

--- config/server.js
+++ config/server.js
@@ -2,6 +2,11 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'APP_KEY_1',
+      'APP_KEY_2',
+      'APP_KEY_3',
+      'APP_KEY_4'
+    ]),
   },
 });
