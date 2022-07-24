2b95478104b4d3be7a67a0cb5fecee479a280902
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-23 21:19:52 -0300

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
+  connection: env('DATABASE_DRIVE', 'sqlite') === 'sqlite' ?
+    {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_URL', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    }
+    : env('DATABASE_DRIVE') === 'postgres' ?
+    {
+      client: 'postgres'
+    }
+    : ['mariadb', 'mysql'].includes(env('DATABASE_DRIVE')) &&
+    {
+      client: 'mysql'
+    }
 });
